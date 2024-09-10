72e70695d1e6afdfa1f31bc89f312a5ebb398b71
thangtran1904 <168847723+thangtran1904@users.noreply.github.com>
2024-09-10 21:50:56 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,48 +1,20 @@
 ﻿using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
-namespace ConsoleApp222
+namespace WindowsFormsTaxi
 {
-    internal class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            //bài toán : Phần mềm tính tiền taxi
-            // giải thuật:
-            //input : quãng đường = s,giá mỗi km =a,giá mở cửa b
-            //output : thành tiền q or báo lỗi
-            // thuật toán 
-            // 1 kiểm tra quãng đường với giá tiền phải là số dương
-            // thỏa mãn qua b2
-            // 2. áp dụng công thức q = b + (s*a)
-            // output là q
-            // 3. output là báo lỗi
-            Console.InputEncoding = Encoding.UTF8;
-            Console.OutputEncoding = Encoding.UTF8;
-
-            Console.WriteLine("Phần mềm tính tiền taxi -ConsoleApp222");
-
-            double giaMoCua = 9000;
-
-            Console.Write("Nhập quãng đường (đơn vị km): ");
-            double quangDuong;
-            bool isValidQuangDuong = double.TryParse(Console.ReadLine(), out quangDuong);
-
-            Console.Write("Nhập giá mỗi km: ");
-            double giaMoiKm;
-            bool isValidGiaMoiKm = double.TryParse(Console.ReadLine(), out giaMoiKm);
-
-            if (isValidQuangDuong && isValidGiaMoiKm && quangDuong > 0 && giaMoiKm > 0)
-            {
-                double thanhTien = giaMoCua + (quangDuong * giaMoiKm);
-                Console.WriteLine("Tổng tiền taxi: " + thanhTien.ToString("0.00") + " VND");
-            }
-            else
-            {
-                Console.WriteLine("Lỗi: Vui lòng nhập số dương cho quãng đường và giá mỗi km.");
-            }
-
-            Console.ReadKey();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
